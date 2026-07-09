372abd93b99ce2b386229b76beaf0e17ff54244e
Tokyo-Boy <haveyeverett@gmail.com>
2026-07-08 18:32:06 -0700

    Added images

--- custom.js
+++ custom.js
@@ -3,7 +3,11 @@
 const customImages = [
     "images/photo1.jpg", 
     "images/photo2.png", 
-    "images/photo3.jpg"
+    "images/photo3.jpg",
+    "images/photo4.jpg",
+    "images/photo5.jpg",
+    "images/photo6.jpg",
+    "images/photo7.jpg"
 ];
 
 const customBulletPoints = [
