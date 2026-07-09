a663db07e4737010e562dd1b62bb3c89c2d9082b
Tokyo-Boy <haveyeverett@gmail.com>
2026-07-08 18:01:46 -0700

    Updated

---
+++ custom.js
@@ -0,0 +1,17 @@
+// custom.js - Add your own inside family jokes and photos here!
+
+const customImages = [
+    "photo1.jpg", 
+    "photo2.png", 
+    "photo3.jpg"
+];
+
+const customBulletPoints = [
+    "This explains why Dad falls asleep on the couch at 7:00 PM.",
+    "Mom explicitly told me not to put this in the presentation.",
+    "According to the youngest sibling, this is completely fair.",
+    "This is the real reason we ran out of snacks last Tuesday.",
+    "Scientifically proven to be more annoying than doing the dishes.",
+    "If found, please return this slide to Grandma.",
+    "Phase 2 of this plan involves ordering too much pizza."
+];
